8fba072460c8e9100012308f6dab18de1ed0540e
saasbook <saasbook@saasbook.(none)>
2012-06-17 20:29:51 -0700

    hw3 part1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  regexp = /#{string1}.*#{string2}/m #  /m means match across newlines
+  page.body.should =~ regexp
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
